886e320e26b7df39b04a252150b0da0b7403ba69
Vibhabharti <vibahbharti@gmail.com>
2022-07-13 15:17:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import { Suspense } from 'react';
+import Login from './pages/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
+
+function App() {
+  return (
+<Router>
+  <Routes>
+    <Route exact path='/' element={
+      <Suspense fallback={<div>Loding....</div>}>
+    <Login/>
+    </Suspense>
+    }/>
+  </Routes>
+</Router> 
+    );
+}
+
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,107 @@
+import React, {useState} from 'react'
+import { Dropdown,DropdownButton} from 'react-bootstrap'
+import {userSignup} from '../api/auth'
+
+function Login() {
+  const[ showSignup, setShowSignup] = useState(false);
+  const [userType, setuserType] = useState("CUSTOMER");
+  const [userSignupData, setUserSignupData]= userSignup({})
+
+  const toggleSingup =()=>{
+    setShowSignup(!showSignup)
+
+
+  }
+  const handleSelect = (e) => {
+      setuserType(e)
+  }
+
+  const updateSignupData=(e)=> {
+    userSignupData[e.target.id]=e.target.value;
+    console.log(userSignupData);
+  }
+   const signupFn=()=>
+   {
+    return "hello"
+   }
+    
+  
+  
+  return (
+    <div className='bg-primary d-flex justify-content-center align-items-center vh-100'>
+      <div className="card m-5 p-5">
+        <div className="row">
+          <div className="col">
+            {
+              !showSignup ? (
+<div className="Login"> 
+<form> <div className='input-group m-1'>
+  <input type="text" className="form-control" placeholder='User ID' />
+  </div>
+
+  <div className='input-group m-1'>
+  <input type="text" className="form-control" placeholder='Password' />
+  </div>
+  
+  <div className='input-group m-1'>
+  <input type="Submit" className="form-control btn btn-primary" value="Log in" />
+  </div>
+  <div className="text-info text-center" onClick={toggleSingup}> Don't have an account? Signup</div>
+   </form>
+
+</div>
+              ):(
+
+                <div className="SignUp">SignUp
+<form onSubmit={signupFn}>
+   <div className='input-group m-1'>
+  <input type="text" className="form-control" placeholder='User ID' id='userid' onChange={updateSignupData} />
+  </div>
+  <div className='input-group m-1'>
+  <input type="text" className="form-control" placeholder='UserName' id="username" onChange={updateSignupData} />
+  </div>
+ < div className='input-group m-1'>
+  <input type="Email" className="form-control" placeholder='Email' id='email' onChange={updateSignupData} />
+  </div>
+  <div className='input-group m-1'>
+  <input type="Number" className="form-control" placeholder= 'Age' id='age' onChange={updateSignupData} />
+  </div>
+  <div className='input-group m-1'>
+  <input type="Mobile-number" className="form-control" placeholder='Mobile Number' id='mobilenumber' onChange={updateSignupData} />
+  </div>
+
+  <div className='input-group m-1'>
+  <input type="text" className="form-control" placeholder='Password' id='password' onChange={updateSignupData} />
+  </div>
+  <div className='input-group m-1'>
+  <span type="text-muted">User Type </span>
+  <DropdownButton
+   align="end"
+   title={userType}
+   variant="light"
+   className="mx-1"
+  onSelect={handleSelect}>
+    
+<Dropdown.Item eventKey="CUSTOMER">CUSTOMER</Dropdown.Item>
+<Dropdown.Item eventKey="ENGINEER">ENGINEER</Dropdown.Item>
+    
+  </DropdownButton>
+  </div>
+   <div className='input-group m-1'>
+  <input type="Submit" className="form-control btn btn-primary" value="Sign Up" />
+  </div>
+  <div className="text-info text-center" onClick={toggleSingup}> Already Have an account? Login</div>
+   </form>
+
+</div>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  )
+}
+
+export default Login;
